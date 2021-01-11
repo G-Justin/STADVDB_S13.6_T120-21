c7a6d00f39019de4a9b96eed2fc1ee7050e2b5ff
Justin <justingalura@gmail.com>
2021-01-11 19:51:28 +0800

    clean test

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -16,62 +16,6 @@ jQuery( function () {
         alert('hello')
         window.open('/movie/' + id);
     }
-
-    /*
-    $('#year-limit-form').on('submit', (e) => {
-        
-        const errorMessage = $('#error-message');
-        const year = $('#year-input').val();
-        const limit = $('#limit-input').val();
-
-        if (year.trim() !== "") {
-            if (Number(year) > MAX_YEAR || Number(year) < MIN_YEAR) {
-                errorMessage.text('Year exceeds 1951 - 2017 range!');
-                e.preventDefault();
-                return;
-            }
-        }
-
-        if (limit.trim() === "") {
-            if (Number(limit) > MAX_LIMIT || Number(limit) < MIN_LIMIT) {
-                errorMessage.text('Invalid limit input!');
-                e.preventDefault();
-                return;
-            }
-        }
-
-    }); */
-
-    /*
-    $('#genre-limit-form').on('submit', (e) => {
-        const genre = $('#genre-input').val().trim();
-        const limit = $('#limit-input').val().trim();
-
-        if (!GENRES.includes(genre)) {
-            //TODO: Error message
-            e.preventDefault();
-        }
-
-        if (limit > MAX_LIMIT || limit < MIN_LIMIT) {
-
-            e.preventDefault();
-        }
-    });
-    */
-
-    /*
-    $('#keyword-form').on('submit', (e) => {
-        const keyword = $('#keyword-input').val().trim();
-
-        if (keyword.length > MAX_STRING_LENGTH) {
-            e.preventDefault();
-        }
-    }) */
-
-
-
-
-
-
+    
 } );
 
