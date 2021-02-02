a72f1349aa54335df19c3846f16af37065efb9e1
Justin <justingalura@gmail.com>
2021-02-03 06:44:11 +0800

    changed db host

--- models/database.js
+++ models/database.js
@@ -13,10 +13,10 @@ const connect = async () => {
 
   const pgconnection = new Pool({
     user: "avnadmin",
-    password: "bcba6pfw9v5g6i68",
-    host: "pg-2e07fd4b-dlsu-26dd.aivencloud.com",
+    password: "jkfirhh08d4sywvv",
+    host: "pg-9ad65a1-justingalura-cb35.aivencloud.com",
     database: "db_movies",
-    port: 14205,
+    port: 11669,
     idleTimeoutMillis: 0,
     connectionTimeoutMillis: 0,
     ssl: {rejectUnauthorized: false}
@@ -24,10 +24,10 @@ const connect = async () => {
 
   const olapconnection = new Pool({
     user: "avnadmin",
-    password: "bcba6pfw9v5g6i68",
-    host: "pg-2e07fd4b-dlsu-26dd.aivencloud.com",
+    password: "jkfirhh08d4sywvv",
+    host: "pg-9ad65a1-justingalura-cb35.aivencloud.com",
     database: "dw_movies",
-    port: 14205,
+    port: 11669,
     idleTimeoutMillis: 0,
     connectionTimeoutMillis: 0,
     ssl: {rejectUnauthorized: false}
